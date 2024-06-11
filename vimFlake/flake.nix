2c23f6aa5557ab238befe5e36b0c1ef340901fa3
DumbDane <lrpe@itu.dk>
2024-06-11 19:16:15 +0200

    First flake

---
+++ vimFlake/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let 
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default =
+        pkgs.mkShell
+	  {
+	    buildInputs = [
+	      pkgs.neovim
+	      pkgs.vim
+	    ];
+
+	    shellHook = ''
+	      echo "hello dad"
+	    '';
+
+	  };
+
+    };
+
+}
